b399a1a90a49305a5c2020b32d101f471b260c72
Jay <chlwodnjs34@gmail.com>
2023-11-16 18:02:43 +0900

    #8 refactor : 포트폴리오에 필요한 계산 util 함수 추가

--- src/utils/calculations.ts
+++ src/utils/calculations.ts
@@ -1,9 +1,25 @@
 export const calculateRate = (num1: number, num2: number) => {
-  return ((num1 - num2) / num2) * 100;
+  const rate = ((num1 - num2) / num2) * 100;
+
+  return formatValue(rate);
+};
+
+export const calculateLossRate = (num1: number, num2: number) => {
+  const rate = ((num1 - num2) / num1) * 100;
+
+  return formatValue(rate);
 };
 
 export const calculateValue = (rate: number, base: number) => {
   const value = base + (rate / 100) * base;
 
-  return Math.floor(value) === value ? value.toString() : value.toFixed(2);
+  return formatValue(value);
 };
+
+function formatValue(value: number) {
+  if (value % 1 === 0) {
+    return value.toString();
+  } else {
+    return parseFloat(value.toFixed(2)).toString();
+  }
+}
