b4f71c964a0762f032b734df15ef34c171afb825
Jay <chlwodnjs34@gmail.com>
2023-11-22 17:41:07 +0900

    #19 feat : Query/Mutation Cache에서 에러 일괄 처리

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,16 @@ import { UserProvider } from "@context/UserContext.tsx";
 import { StyledEngineProvider } from "@mui/styled-engine";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { createToast } from "@utils/toast/createToast.ts";
+import { AxiosError } from "axios";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -16,7 +24,36 @@ if (process.env.NODE_ENV === "development") {
   });
 }
 
-const queryClient = new QueryClient();
+const toast = createToast();
+
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      if (query.meta?.errorMessage) {
+        toast.error(query.meta.errorMessage as string);
+        return;
+      }
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message);
+        return;
+      }
+      toast.error(String(error));
+    },
+  }),
+  mutationCache: new MutationCache({
+    onError: (error, _, __, mutation) => {
+      if (mutation.meta?.errorMessage) {
+        toast.error(mutation.meta.errorMessage as string);
+        return;
+      }
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message);
+        return;
+      }
+      toast.error(String(error));
+    },
+  }),
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
