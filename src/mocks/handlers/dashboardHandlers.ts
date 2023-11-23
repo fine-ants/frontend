22596dc32a02bf1f919cd5278bb7da9bd2ceae4a
bakhacode <bakhamitai@naver.com>
2023-11-23 18:49:36 +0900

    feat: 대시보드 api msw 적용

---
+++ src/mocks/handlers/dashboardHandlers.ts
@@ -0,0 +1,32 @@
+import { HTTPSTATUS } from "@api/types";
+import {
+  successfulGetOverviewDataResponse,
+  successfulGetPortfolioPieChartDataResponse,
+  successfulGetTotalValuationDataResponse,
+} from "mocks/data/dashboardData";
+import { rest } from "msw";
+
+export default [
+  // Get dashboard overview data
+  rest.get("/api/dashboard/overview", async (_, res, ctx) => {
+    return res(
+      ctx.status(HTTPSTATUS.success),
+      ctx.json(successfulGetOverviewDataResponse)
+    );
+  }),
+  // Get portfolio pie chart data
+  rest.get("/api/dashboard/pieChart", async (_, res, ctx) => {
+    return res(
+      ctx.status(HTTPSTATUS.success),
+      ctx.json(successfulGetPortfolioPieChartDataResponse)
+    );
+  }),
+
+  // Get total valuation line chart data
+  rest.get("/api/dashboard/lineChart", async (_, res, ctx) => {
+    return res(
+      ctx.status(HTTPSTATUS.success),
+      ctx.json(successfulGetTotalValuationDataResponse)
+    );
+  }),
+];
