586df334873441d40a3dd8587326dd901e67eb76
bakhacode <bakhamitai@naver.com>
2023-11-23 18:52:40 +0900

    refactor: 대시보드 파이 차트 분리

---
+++ src/components/Dashboard/DashboardPieChart.tsx
@@ -0,0 +1,36 @@
+import useDashboardPieChartQuery from "@api/dashboard/queries/useDashboardPieChartQuery";
+import Legend from "@components/common/PieChart/Legend";
+import styled from "styled-components";
+import PortfolioPieChart from "./PortfolioPieChart";
+
+export default function DashboardPieChart() {
+  const { data: pieData } = useDashboardPieChartQuery();
+
+  return (
+    <StyledDashboardPieChart>
+      <ChartTitle>포트폴리오 비중</ChartTitle>
+      <div style={{ display: "flex", gap: "24px" }}>
+        <PortfolioPieChart width={320} height={320} pieData={pieData ?? []} />
+        <Legend pieData={pieData ?? []} />
+      </div>
+    </StyledDashboardPieChart>
+  );
+}
+
+const StyledDashboardPieChart = styled.div`
+  width: 708px;
+  height: 480px;
+  background-color: #ffffff;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  padding: 32px;
+  gap: 24px;
+`;
+
+const ChartTitle = styled.div`
+  font-size: 24px;
+  line-height: 29px;
+  font-weight: bold;
+`;
