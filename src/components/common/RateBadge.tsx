a764dc97f6f7440a0f543dd5bf42e2621ae1d8df
bakhacode <bakhamitai@naver.com>
2023-11-29 16:01:13 +0900

    feat: RateBadge 컴포넌트 구현

---
+++ src/components/common/RateBadge.tsx
@@ -0,0 +1,91 @@
+import downIcon from "@assets/icons/ic_down.svg";
+import noneIcon from "@assets/icons/ic_none.svg";
+import upIcon from "@assets/icons/ic_up.svg";
+import designSystem from "@styles/designSystem";
+import styled from "styled-components";
+
+type Props = {
+  rate: number;
+  bgColorStatus?: boolean;
+  iconStatus?: boolean;
+  isDividend?: boolean;
+};
+
+export default function RateBadge({
+  rate,
+  bgColorStatus = true,
+  iconStatus = true,
+  isDividend = false,
+}: Props) {
+  const getColor = (value: number, isDividendRate: boolean) => {
+    // 배당금일시 바로 오렌지 색 리턴
+    if (isDividendRate) {
+      return {
+        color: designSystem.color.state.orange,
+        bgColor: designSystem.color.state.orange16,
+      };
+    }
+
+    // 배당금이 아닌 경우 상승 하락에 따른 색깔 리턴
+    if (value > 0) {
+      return {
+        color: designSystem.color.state.green,
+        bgColor: designSystem.color.state.green16,
+      };
+    } else if (value === 0) {
+      return {
+        color: designSystem.color.neutral.gray400,
+        bgColor: designSystem.color.neutral.gray40016,
+      };
+    } else if (value < 0) {
+      return {
+        color: designSystem.color.state.red,
+        bgColor: designSystem.color.state.red16,
+      };
+    } else {
+      return {
+        color: designSystem.color.neutral.gray400,
+        bgColor: designSystem.color.neutral.gray40016,
+      };
+    }
+  };
+
+  const getIconSrc = (value: number) => {
+    if (value > 0) {
+      return upIcon;
+    } else if (value === 0) {
+      return noneIcon;
+    } else if (value < 0) {
+      return downIcon;
+    } else {
+      return noneIcon;
+    }
+  };
+
+  return (
+    <StyledRateBadge
+      $color={getColor(rate, isDividend)}
+      $bgColorStatus={bgColorStatus}>
+      {iconStatus ? <img src={getIconSrc(rate)} alt="rateStatus" /> : null}
+      {rate}%
+    </StyledRateBadge>
+  );
+}
+
+const StyledRateBadge = styled.div<{
+  $color: { color: string; bgColor: string };
+  $bgColorStatus: boolean;
+}>`
+  flex-grow: 0;
+  display: flex;
+  gap: 2.5px;
+  align-items: center;
+  justify-content: center;
+  height: 24px;
+  font: ${({ theme: { font } }) => font.title5};
+  color: ${({ $color }) => $color.color};
+  background-color: ${({ $color, $bgColorStatus }) =>
+    $bgColorStatus ? $color.bgColor : "none"};
+  border-radius: 4px;
+  padding: 3.5px 4px;
+`;
