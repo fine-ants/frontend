9e86a5db8170129d875ce488f67303abc1a5cfee
bakhacode <bakhamitai@naver.com>
2023-11-29 16:28:19 +0900

    style: 사용하지 않는 주석 삭제

--- src/components/common/UserControls.tsx
+++ src/components/common/UserControls.tsx
@@ -5,7 +5,7 @@ import profileImage from "@assets/images/profileImage.png";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import CounterBadge from "./CounterBadge";
+import CounterBadge from "./Badges/CounterBadge";
 
 const count = 9;
 
@@ -66,54 +66,3 @@ const ControlButton = styled.button`
 const NotificationWrapper = styled.div`
   position: relative;
 `;
-
-// const CounterBadge = styled.div<{ $numLength: number }>`
-//   height: 16px;
-
-//   background-color: ${({ theme: { color } }) => color.state.red};
-//   color: white;
-//   font-size: 10px;
-//   font-weight: bold;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   position: absolute;
-//   top: 2px;
-//   right: 1px;
-//   width: ${({ $numLength }) => {
-//     switch ($numLength) {
-//       case 1:
-//         return 16;
-//       case 2:
-//         return 19;
-//       case 3:
-//         return 26;
-//       default:
-//         return 16;
-//     }
-//   }}px;
-//   border-radius: ${({ $numLength }) => {
-//     switch ($numLength) {
-//       case 1:
-//         return 50;
-//       case 2:
-//         return 8;
-//       case 3:
-//         return 8;
-//       default:
-//         return 8;
-//     }
-//   }}px;
-//   transform: ${({ $numLength }) => {
-//     switch ($numLength) {
-//       case 1:
-//         return "translateX(-5%)";
-//       case 2:
-//         return "translateX(10%)";
-//       case 3:
-//         return "translateX(35%)";
-//       default:
-//         return "translateX(50%)";
-//     }
-//   }};
-// `;
