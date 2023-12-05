58ae5fc9c7c8ae85c68b094f8652453c659339ba
Jay <chlwodnjs34@gmail.com>
2023-12-05 19:42:09 +0900

    #40 feat : customTooltip 구현

---
+++ src/components/common/AutoTooltipText.tsx
@@ -0,0 +1,44 @@
+import { ReactNode, useEffect, useRef, useState } from "react";
+import { CustomTooltip } from "./CustomTooltip";
+
+type Props = {
+  children: ReactNode;
+  maxWidth: string;
+};
+
+export const AutoTooltipText = ({ children, maxWidth }: Props) => {
+  const [isTextOverflow, setIsTextOverflow] = useState(false);
+  const textRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const compareSize = () => {
+      if (textRef.current) {
+        const isOverflow =
+          textRef.current.scrollWidth > textRef.current.clientWidth;
+        setIsTextOverflow(isOverflow);
+      }
+    };
+
+    window.addEventListener("resize", compareSize);
+    compareSize();
+
+    return () => {
+      window.removeEventListener("resize", compareSize);
+    };
+  }, [children]);
+
+  return (
+    <CustomTooltip smallPadding title={isTextOverflow ? children : ""}>
+      <div
+        ref={textRef}
+        style={{
+          maxWidth: maxWidth,
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+        }}>
+        {children}
+      </div>
+    </CustomTooltip>
+  );
+};
