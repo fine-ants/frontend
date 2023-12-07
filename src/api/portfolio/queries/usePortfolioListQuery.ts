860bbcfbdc830c02442974d2db64b801f47fc7ee
Daeram Chung <daeram.chung@gmail.com>
2023-12-07 16:49:31 +0900

    #43 refactor: 포트폴리오 목록 dropdown 401 임시 처방 수정

--- src/api/portfolio/queries/usePortfolioListQuery.ts
+++ src/api/portfolio/queries/usePortfolioListQuery.ts
@@ -1,29 +1,20 @@
-import { Response } from "@api/types";
-import { UserContext } from "@context/UserContext";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { useContext } from "react";
 import { getPortfoliosList } from "..";
-import { PortfoliosList } from "../types";
 import { portfolioKeys } from "./queryKeys";
 
-export default function usePortfolioListQuery() {
-  // TOOD: 임시 처방
-  // 로그인하지 않았을 때도 Header 컴포넌트에서 포트폴리오 목록을 불러오기 때문 (401 error 발생).
-  const { user } = useContext(UserContext);
-
+export default function usePortfolioListQuery(shouldNotFetch?: boolean) {
   return useSuspenseQuery({
     queryKey: portfolioKeys.list().queryKey,
     queryFn: () => {
-      return new Promise<Response<PortfoliosList>>((resolve) => {
-        if (!user)
-          return resolve({
-            code: 200,
-            status: "",
-            message: "",
-            data: { portfolios: [] },
-          });
-        resolve(getPortfoliosList());
-      });
+      if (shouldNotFetch) {
+        return {
+          code: 200,
+          status: "",
+          message: "",
+          data: { portfolios: [] },
+        };
+      }
+      return getPortfoliosList();
     },
     retry: false,
     select: (res) => res.data,
