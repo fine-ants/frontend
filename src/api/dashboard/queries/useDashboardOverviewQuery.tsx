7f15891af15a09c81496f81802b37d2d261d323f
Jay <chlwodnjs34@gmail.com>
2023-12-08 02:20:15 +0900

    #44 refactor : errorBoundary를 위한 수정
    - useQuery를 useSuspenseQuery로 변경
    - retry 0으로 변경

--- src/api/dashboard/queries/useDashboardOverviewQuery.tsx
+++ src/api/dashboard/queries/useDashboardOverviewQuery.tsx
@@ -1,14 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { getDashboardOverview } from "..";
 import { dashboardKeys } from "./queryKeys";
 
 export default function useDashboardOverviewQuery() {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: dashboardKeys.overview().queryKey,
     queryFn: getDashboardOverview,
     select: (res) => res.data,
     meta: {
       errorMessage: "오버뷰 정보를 불러오는데 실패했습니다",
     },
+    retry: 0,
   });
 }
