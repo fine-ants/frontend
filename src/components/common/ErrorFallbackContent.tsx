4abf5b624d663e254f44ba1c0465b525a4809a57
Jay <chlwodnjs34@gmail.com>
2023-12-08 15:30:08 +0900

    #44 refactor : 불필요한 error message 삭제

--- src/components/common/ErrorFallbackContent.tsx
+++ src/components/common/ErrorFallbackContent.tsx
@@ -2,15 +2,11 @@ import { Button } from "@mui/material";
 import { FallbackProps } from "react-error-boundary";
 import styled from "styled-components";
 
-export function ErrorFallbackContent({
-  error,
-  resetErrorBoundary,
-}: FallbackProps) {
+export function ErrorFallbackContent({ resetErrorBoundary }: FallbackProps) {
   return (
     <>
       <Title>서버로부터 데이터를 불러오지 못했습니다.</Title>
       <Content>새로고침을 하거나 잠시 후 다시 시도해 주세요.</Content>
-      <div>{error.message}</div>
       <Button variant="contained" onClick={resetErrorBoundary}>
         새로고침
       </Button>
