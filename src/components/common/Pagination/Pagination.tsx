a2dbe583bfdf8131c25bd75ae380d9011fc774c1
Daeram Chung <daeram.chung@gmail.com>
2023-12-12 19:02:51 +0900

    #30 feat: Pagination component 구현

--- src/components/common/Pagination/Pagination.tsx
+++ src/components/common/Pagination/Pagination.tsx
@@ -1,26 +1,73 @@
 import chevronLeftIcon from "@assets/icons/ic_chevron-left.svg";
 import chevronRightIcon from "@assets/icons/ic_chevron-right.svg";
-import { Pagination as MuiPagination, PaginationItem } from "@mui/material";
+import {
+  Pagination as MuiPagination,
+  PaginationItem,
+  paginationClasses,
+} from "@mui/material";
+import designSystem from "@styles/designSystem";
+import { ChangeEvent } from "react";
+import styled from "styled-components";
 
 type Props = {
   count: number;
+  page: number;
+  onPageChange: (event: ChangeEvent<unknown>, page: number) => void;
 };
 
-export default function Pagination({ count }: Props) {
+export default function Pagination({ count, page, onPageChange }: Props) {
   return (
-    <MuiPagination
+    <StyledPagination
       count={count}
+      page={page}
+      onChange={(event: ChangeEvent<unknown>, newPage: number) => {
+        onPageChange(event, newPage - 1);
+      }}
       shape="rounded"
       renderItem={(item) => (
         <PaginationItem
           slots={{
-            previous: () => <img src={chevronLeftIcon} />,
-            next: () => <img src={chevronRightIcon} />,
+            previous: () => <ChevronImg src={chevronLeftIcon} />,
+            next: () => <ChevronImg src={chevronRightIcon} />,
           }}
           {...item}
         />
       )}
-      sx={{ height: "24px" }}
     />
   );
 }
+
+const StyledPagination = styled(MuiPagination)`
+  height: 24px;
+
+  .${paginationClasses.ul} {
+    gap: 8px;
+
+    > li {
+      width: 24px;
+      height: 24px;
+
+      > button {
+        width: 24px;
+        min-width: 24px;
+        height: 24px;
+        margin: 0;
+        padding: 0;
+
+        &.Mui-selected {
+          background-color: ${designSystem.color.primary.blue50};
+          color: ${designSystem.color.primary.blue500};
+        }
+
+        &:hover {
+          background-color: ${designSystem.color.neutral.gray50};
+        }
+      }
+    }
+  }
+`;
+
+const ChevronImg = styled.img`
+  width: 16px;
+  height: 16px;
+`;
