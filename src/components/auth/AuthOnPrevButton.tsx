5a70077938914514373f59dd848ffafe712afd1e
Jay <chlwodnjs34@gmail.com>
2023-12-13 01:36:20 +0900

    #47 feat : 뒤로가기 버튼 공용 컴포넌트 구현

---
+++ src/components/auth/AuthOnPrevButton.tsx
@@ -0,0 +1,27 @@
+import arrow from "@assets/icons/arrow.svg";
+import styled from "styled-components";
+
+type Props = {
+  onPrev: () => void;
+};
+
+export function AuthOnPrevButton({ onPrev }: Props) {
+  return (
+    <div>
+      <StyledButton onClick={onPrev}>
+        <img src={arrow} />
+      </StyledButton>
+    </div>
+  );
+}
+
+const StyledButton = styled.button`
+  display: flex;
+  width: 24px;
+  height: 24px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  flex-shrink: 0;
+  padding: 0;
+`;
