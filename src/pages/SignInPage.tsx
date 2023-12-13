56521c7ba6f6b29d4fdd1b584393d6d54e00bdf6
Jay <chlwodnjs34@gmail.com>
2023-12-13 23:32:56 +0900

    #47 refactor : 테스트로 주석 처리된 로직 롤백

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -74,7 +74,7 @@ export default function SignInPage() {
         if (provider && authCode && state) {
           await oAuthSignInMutate({ provider, authCode, state });
         }
-        // closePopUpWindow();
+        closePopUpWindow();
       }
     };
 
