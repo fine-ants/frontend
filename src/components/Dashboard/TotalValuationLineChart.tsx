c35ac0ec08fc8e2225674bd9fa85e0530a0a5216
Jay <chlwodnjs34@gmail.com>
2023-12-15 15:51:08 +0900

    #56 refactor : Tab 사용처 수정 및 불필요한 styled 삭제

--- src/components/Dashboard/TotalValuationLineChart.tsx
+++ src/components/Dashboard/TotalValuationLineChart.tsx
@@ -1,6 +1,7 @@
 import { useLCLineChart } from "@components/hooks/useLCLineChart";
 import { LineData } from "lightweight-charts";
 import { useRef } from "react";
+import styled from "styled-components";
 
 type Props = {
   data: LineData[];
@@ -14,15 +15,15 @@ export default function TotalValuationLineChart({
   currentRangeIndex,
 }: Props) {
   const chartContainerRef = useRef<HTMLDivElement>(null);
-
   const currentRange = rangeMapping[currentRangeIndex];
 
   useLCLineChart({ chartContainerRef, data, currentRange });
 
-  return (
-    <div
-      ref={chartContainerRef}
-      style={{ width: "100%", height: "100%", overflow: "hidden" }}
-    />
-  );
+  return <StyledLineChart ref={chartContainerRef} />;
 }
+
+const StyledLineChart = styled.div`
+  width: "100%";
+  overflow: "hidden";
+  flex: 1;
+`;
