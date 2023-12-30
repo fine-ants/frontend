237df6357408c522b3249024e2e092c914b680d9
Daeram Chung <daeram.chung@gmail.com>
2023-12-30 15:37:57 +0900

    #70 feat: WatchlistTableSkeleton 구현

--- src/components/Watchlist/skeletons/WatchlistTableSkeleton.tsx
+++ src/components/Watchlist/skeletons/WatchlistTableSkeleton.tsx
@@ -1,3 +1,46 @@
+import { Skeleton } from "@mui/material";
+import styled from "styled-components";
+
 export default function WatchlistTableSkeleton() {
-  return <div>skeleton</div>;
+  return (
+    <StyledWatchlistTableSkeleton>
+      <WatchlistTableToolBarSkeleton variant="rounded" />
+
+      <WatchlistTableHeadSkeleton variant="rounded" />
+
+      {Array.from({ length: 5 }).map((_, idx) => (
+        <WatchlistTableRowSkeleton key={idx} variant="rectangular" />
+      ))}
+
+      <WatchlistTablePaginationSkeleton variant="rectangular" />
+    </StyledWatchlistTableSkeleton>
+  );
 }
+
+const StyledWatchlistTableSkeleton = styled.div`
+  width: 100%;
+`;
+
+const WatchlistTableToolBarSkeleton = styled(Skeleton)`
+  width: 100%;
+  height: 32px;
+  margin-bottom: 16px;
+`;
+
+const WatchlistTableHeadSkeleton = styled(Skeleton)`
+  width: 100%;
+  height: 48px;
+  margin-bottom: 8px;
+`;
+
+const WatchlistTableRowSkeleton = styled(Skeleton)`
+  width: 100%;
+  height: 64px;
+  border-bottom: 1px solid ${({ theme: { color } }) => color.neutral.gray100};
+`;
+
+const WatchlistTablePaginationSkeleton = styled(Skeleton)`
+  width: 100%;
+  height: 24px;
+  margin-top: 16px;
+`;
