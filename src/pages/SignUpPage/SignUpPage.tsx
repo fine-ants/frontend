3756c38e72fff8b08261d5d040ae46001bf64aac
jay <chlwodnjs34@gmail.com>
2024-01-04 14:51:46 +0900

    #77 refactor : 메인 next 버튼 수정

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -55,7 +55,7 @@ export default function SignUpPage() {
         <SubPageContainer>
           <Funnel>
             <Funnel.Step name="main">
-              <MainSubPage onNext={() => changeStep("profileImage")} />
+              <MainSubPage onNext={() => changeStep("nickname")} />
             </Funnel.Step>
 
             <Funnel.Step name="nickname">
