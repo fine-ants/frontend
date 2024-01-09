0b683ab33bcb6de6d29e63732afc41fe8979c0e2
Daeram Chung <daeram.chung@gmail.com>
2024-01-09 15:02:19 +0900

    sse test

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,6 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -55,16 +54,16 @@ const queryClient = new QueryClient({
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <QueryClientProvider client={queryClient}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <UserProvider>
-            <App />
-          </UserProvider>
-        </LocalizationProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </StyledEngineProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <StyledEngineProvider injectFirst>
+    <QueryClientProvider client={queryClient}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </LocalizationProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StyledEngineProvider>
+  // </React.StrictMode>
 );
--- src/pages/Portfolio/PortfolioPage.tsx
+++ src/pages/Portfolio/PortfolioPage.tsx
@@ -1,3 +1,5 @@
+import { useSSE } from "@api/hooks/useSSE";
+import { PortfolioSSE } from "@api/portfolio/types";
 import ChartsPanel from "@components/Portfolio/Charts/ChartsPanel";
 import ChartsPanelErrorFallback from "@components/Portfolio/Charts/errorFallback/ChartsPanelErrorFallback";
 import ChartsPanelSkeleton from "@components/Portfolio/Charts/skeletons/ChartsPanelSkeleton";
@@ -5,19 +7,24 @@ import MainPanel from "@components/Portfolio/MainPanel";
 import MainPanelSkeleton from "@components/Portfolio/MainPanelSkeleton";
 import MainPanelErrorFallback from "@components/Portfolio/errorFallback/MainPanelErrorFallback";
 import { AsyncBoundary } from "@components/common/AsyncBoundary";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import BasePage from "../BasePage";
 
 export default function PortfolioPage() {
-  // const { portfolioId } = useParams();
-  // const {
-  //   data: portfolioSSE,
-  //   isLoading,
-  //   isError,
-  // } = useSSE<PortfolioSSE>({
-  //   url: `/api/portfolio/${portfolioId}/holdings/realtime`,
-  //   eventTypeName: "portfolioDetails",
-  // });
+  const { portfolioId } = useParams();
+  const {
+    data: portfolioSSE,
+    isLoading,
+    isError,
+  } = useSSE<PortfolioSSE>({
+    url: `/api/portfolio/${portfolioId}/holdings/realtime`,
+    eventTypeName: "portfolioDetails",
+  });
+
+  console.log("portfolioSSE", portfolioSSE);
+  console.log("isLoading", isLoading);
+  console.log("isError", isError);
 
   return (
     <BasePage>
