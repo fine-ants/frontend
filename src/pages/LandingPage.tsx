0605286bd9532d1d4a02c16b1f93dc51be572c65
Daeram Chung <daeram.chung@gmail.com>
2024-01-11 14:49:46 +0900

    Add temporary landing page nav

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,19 @@
 import { Typography } from "@mui/material";
+import Routes from "@router/Routes";
+import { useNavigate } from "react-router-dom";
 import BasePage from "./BasePage";
 
 export default function LandingPage() {
+  const navigate = useNavigate();
+
   return (
     <BasePage>
+      <nav>
+        <ul>
+          <li onClick={() => navigate(Routes.SIGNIN)}>로그인</li>
+          <li onClick={() => navigate(Routes.SIGNUP)}>회원가입</li>
+        </ul>
+      </nav>
       <Typography variant="h1">Landing Page!</Typography>
     </BasePage>
   );
