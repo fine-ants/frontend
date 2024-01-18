39054a96620dabcb52cf1c709e2129485886e309
Jay <chlwodnjs34@gmail.com>
2024-01-18 17:23:32 +0900

    #111 refactor : toast alert 디자인 적용

--- src/components/common/toast/CustomToastContainer.tsx
+++ src/components/common/toast/CustomToastContainer.tsx
@@ -1,23 +1,50 @@
+import designSystem from "@styles/designSystem";
 import { ToastContainer } from "react-toastify";
 import styled from "styled-components";
+import { CustomToastCloseButton } from "./CustomToastCloseButton";
+
+export function CustomToastContainer() {
+  return <StyledToastContainer closeButton={CustomToastCloseButton} />;
+}
 
 // TODO : 디자인에 맞게 커스터마이징
-export const CustomToastContainer = styled(ToastContainer)`
+const StyledToastContainer = styled(ToastContainer)`
   .Toastify__toast {
+    display: flex;
+    width: 328px;
+    height: 64px;
+    padding: 0px 16px;
+    top: 130px;
+    right: 16px;
+    justify-content: space-between;
+    align-items: center;
+    flex-shrink: 0;
+    color: ${designSystem.color.neutral.gray800};
+    font: ${designSystem.font.title5};
+  }
+
+  .Toastify__toast-containert {
+    top: 8rem !important;
   }
 
   .Toastify__toast-icon {
+    width: 24px;
+    height: 24px;
   }
 
   .Toastify__toast--info {
+    background-color: ${designSystem.color.primary.blue50};
   }
 
   .Toastify__toast--success {
+    background-color: ${designSystem.color.state.green50};
   }
 
   .Toastify__toast--error {
+    background-color: ${designSystem.color.state.red50};
   }
 
   .Toastify__toast--warning {
+    background-color: ${designSystem.color.state.green50};
   }
 `;
