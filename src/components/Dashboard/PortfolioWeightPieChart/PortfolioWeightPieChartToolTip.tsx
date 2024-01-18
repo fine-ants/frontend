9a75a5ad1e6e418270fb0a9901cb49095253bef1
Jay <chlwodnjs34@gmail.com>
2024-01-18 19:34:07 +0900

    #111 refactor : 오타 수정

--- src/components/Dashboard/PortfolioWeightPieChart/PortfolioWeightPieChartToolTip.tsx
+++ src/components/Dashboard/PortfolioWeightPieChart/PortfolioWeightPieChartToolTip.tsx
@@ -89,5 +89,5 @@ const GainValue = styled.p`
   display: flex;
   align-items: center;
   font: ${({ theme: { font } }) => font.body4};
-  color: ${({ theme: { color } }) => color.state500};
+  color: ${({ theme: { color } }) => color.state.green500};
 `;
