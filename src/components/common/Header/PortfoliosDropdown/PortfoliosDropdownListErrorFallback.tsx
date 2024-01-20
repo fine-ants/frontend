b7ca3aa2c82bfca78efb6d53399ba8756b69ec8f
Daeram Chung <daeram.chung@gmail.com>
2024-01-20 22:13:52 +0900

    #106 design: PortfoliosDropdownListErrorFallback 재시도 버튼 UI 수정

--- src/components/common/Header/PortfoliosDropdown/PortfoliosDropdownListErrorFallback.tsx
+++ src/components/common/Header/PortfoliosDropdown/PortfoliosDropdownListErrorFallback.tsx
@@ -1,4 +1,5 @@
 import Button from "@components/common/Buttons/Button";
+import { Icon } from "@components/common/Icon";
 import { Divider } from "@mui/material";
 import { FallbackProps } from "react-error-boundary";
 
@@ -6,14 +7,14 @@ export default function PortfoliosDropdownListErrorFallback({
   resetErrorBoundary,
 }: FallbackProps) {
   return (
-    <div>
-      {/* TODO: Replace with retry icon */}
+    <div style={{ textAlign: "center" }}>
       <Button
         variant="secondary"
         size="h32"
         style={{ margin: "8px auto 12px auto" }}
         onClick={resetErrorBoundary}>
-        재시도
+        <Icon icon="refresh" size={16} color="blue500" />
+        <span>재시도</span>
       </Button>
 
       <Divider />
