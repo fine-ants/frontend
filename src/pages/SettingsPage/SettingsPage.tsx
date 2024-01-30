85fcd176c6462829bf8895b91bbc0b4043b297b2
Daeram Chung <daeram.chung@gmail.com>
2024-01-30 15:52:41 +0900

    #118 feat: SubPageNav 컴포넌트 구현

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,7 +1,8 @@
+import SubPageNav from "@components/common/SubPageNav/SubPageNav";
 import BasePage from "@pages/BasePage";
 import Routes from "@router/Routes";
 import designSystem from "@styles/designSystem";
-import { NavLink, Navigate, useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import styled from "styled-components";
 import AccountSettingsSubPage from "./subPages/AccountSettingsSubPage";
 import ProfileSettingsSubPage from "./subPages/ProfileSettingsSubPage";
@@ -9,6 +10,11 @@ import ProfileSettingsSubPage from "./subPages/ProfileSettingsSubPage";
 function isValidTab(tab: string | undefined) {
   return tab === "profile" || tab === "account";
 }
+
+const subPageNavItems = [
+  { title: "프로필 설정", to: "/settings/profile" },
+  { title: "계정 설정", to: "/settings/account" },
+];
 
 export default function SettingsPage() {
   const { tab } = useParams();
@@ -22,16 +28,7 @@ export default function SettingsPage() {
       <Container>
         <Title>설정</Title>
 
-        <Nav>
-          <ul>
-            <li>
-              <NavLink to="/settings/profile">프로필 설정</NavLink>
-            </li>
-            <li>
-              <NavLink to="/settings/account">계정 설정</NavLink>
-            </li>
-          </ul>
-        </Nav>
+        <SubPageNav navItems={subPageNavItems} />
 
         {tab === "profile" && <ProfileSettingsSubPage />}
         {tab === "account" && <AccountSettingsSubPage />}
@@ -53,41 +50,7 @@ const Container = styled.div`
 
 const Title = styled.div`
   margin-bottom: 24px;
-  font: ${designSystem.font.heading2};
-`;
-
-const Nav = styled.nav`
-  width: 100%;
-  height: 40px;
-  margin-bottom: 40px;
-  border-bottom: 1px solid ${designSystem.color.neutral.gray100};
-
-  > ul {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-
-    > li {
-      width: 120px;
-      height: 100%;
-      margin-bottom: -2px;
-      font: ${designSystem.font.title4};
-      color: ${designSystem.color.neutral.gray600};
-
-      > a {
-        width: 100%;
-        height: 100%;
-        display: block;
-        text-align: center;
-        vertical-align: middle;
-        line-height: 40px;
-      }
-
-      > a.active {
-        border-bottom: 2px solid ${designSystem.color.primary.blue500};
-        color: ${designSystem.color.primary.blue500};
-      }
-    }
-  }
+  font: ${designSystem.font.heading2.font};
+  letter-spacing: ${designSystem.font.heading2.letterSpacing};
+  color: ${designSystem.color.neutral.gray900};
 `;
