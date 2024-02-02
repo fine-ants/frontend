58dfc1ef0242d52a4496c95bc187d53f6ba8672c
Jay <chlwodnjs34@gmail.com>
2024-02-02 18:09:36 +0900

    #118 feat : 기능 로직을 제외한 알림 관련 panel, dialog UI 작업

---
+++ src/components/common/Header/Notification/NotificationPanel.tsx
@@ -0,0 +1,153 @@
+import { Icon } from "@components/common/Icon";
+import { Popover } from "@mui/material";
+import designSystem from "@styles/designSystem";
+import { useState } from "react";
+import styled from "styled-components";
+import { NotificationItem } from "./NotificationItem";
+import { NotificationSettingDialog } from "./NotificationSettingDialog";
+
+type Props = {
+  anchorEl: null | HTMLElement;
+  open: boolean;
+  notifications: {
+    notificationId: number;
+    title: string;
+    content: string;
+    timestamp: string;
+    isRead: boolean;
+  }[];
+  handleClose: () => void;
+};
+
+export function NotificationPanel({
+  anchorEl,
+  open,
+  notifications,
+  handleClose,
+}: Props) {
+  const [isOpenDialog, setIsOPenDialog] = useState(false);
+
+  const openDialog = () => {
+    setIsOPenDialog(true);
+  };
+
+  const closeDialog = () => {
+    setIsOPenDialog(false);
+  };
+
+  return (
+    <>
+      <StyledPopover
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorReference="anchorPosition"
+        anchorPosition={{ top: 70, left: window.innerWidth - 30 }}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}>
+        <StyledNotificationPanel>
+          <PanelHeader>
+            <PanelTitle>알림</PanelTitle>
+            <PanelButtonContainer>
+              <TextButton>활성화 알림 보기</TextButton>
+              <Divider />
+              <button onClick={openDialog}>
+                <Icon icon="settings" color="gray800" size={24} />
+              </button>
+            </PanelButtonContainer>
+          </PanelHeader>
+          <PanelContent>
+            {notifications.map((data) => (
+              <NotificationItem key={data.notificationId} {...data} />
+            ))}
+          </PanelContent>
+          <PanelFooter>
+            <NotificationDeleteAllButton>
+              모든 알림 지우기
+            </NotificationDeleteAllButton>
+          </PanelFooter>
+        </StyledNotificationPanel>
+      </StyledPopover>
+      <NotificationSettingDialog isOpen={isOpenDialog} onClose={closeDialog} />
+    </>
+  );
+}
+
+const StyledPopover = styled(Popover)`
+  .MuiPaper-root {
+    border: 1px solid ${designSystem.color.neutral.gray100};
+    border-radius: 8px;
+  }
+`;
+
+const StyledNotificationPanel = styled.div`
+  width: 384px;
+  min-height: 80vh;
+  height: 820px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const PanelHeader = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 24px;
+`;
+
+const PanelTitle = styled.div`
+  font: ${designSystem.font.heading3.font};
+  letter-spacing: ${designSystem.font.heading3.letterSpacing};
+  color: ${designSystem.color.neutral.gray800};
+  display: flex;
+  align-items: center;
+`;
+
+const PanelButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+`;
+
+const TextButton = styled.button`
+  font: ${designSystem.font.button2.font};
+  letter-spacing: ${designSystem.font.button2.letterSpacing};
+  color: ${designSystem.color.primary.blue500};
+`;
+
+const Divider = styled.div`
+  width: 1px;
+  height: 12px;
+  background-color: ${designSystem.color.neutral.gray200};
+`;
+
+const PanelContent = styled.div`
+  width: 100%;
+  flex: 1;
+  overflow: scroll;
+  padding: 0 8px;
+`;
+
+const PanelFooter = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 0 24px;
+  border-top: 1px solid ${designSystem.color.neutral.gray200};
+`;
+
+const NotificationDeleteAllButton = styled.button`
+  font: ${designSystem.font.button2.font};
+  letter-spacing: ${designSystem.font.button2.letterSpacing};
+  color: ${designSystem.color.neutral.gray600};
+`;
--- src/components/common/Header/UserControls.tsx
+++ src/components/common/Header/UserControls.tsx
@@ -1,22 +1,11 @@
-import notificationIcon from "@assets/icons/ic_notification.svg";
-import designSystem from "@styles/designSystem";
 import styled from "styled-components";
-import CounterBadge from "../Badges/CounterBadge";
+import { Notification } from "./Notification/Notification";
 import UserDropdown from "./UserDropdown";
-
-// TODO: API 만든 후에 remove this
-const count = 10;
 
 export default function UserControls() {
   return (
     <StyledUserControls>
-      <ControlButton>
-        <NotificationWrapper>
-          <img src={notificationIcon} alt="notification" />
-          <CounterBadge count={count} />
-        </NotificationWrapper>
-      </ControlButton>
-
+      <Notification />
       <UserDropdown />
     </StyledUserControls>
   );
@@ -29,23 +18,4 @@ const StyledUserControls = styled.div`
   background-color: inherit;
   margin-left: auto;
   gap: 16px;
-`;
-
-const ControlButton = styled.button`
-  width: 40px;
-  height: 40px;
-  padding: 4px;
-  border-radius: 4px;
-  background-color: inherit;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  &:hover {
-    background-color: ${designSystem.color.neutral.gray800};
-  }
-`;
-
-const NotificationWrapper = styled.div`
-  position: relative;
 `;
