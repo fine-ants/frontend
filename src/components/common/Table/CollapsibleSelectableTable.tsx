c844519850de015e249717e06974912edd56d410
Daeram Chung <daeram.chung@gmail.com>
2024-02-06 15:42:14 +0900

    #118 feat: CollapsibleSelectableTable 구현

--- src/components/common/Table/CollapsibleSelectableTable.tsx
+++ src/components/common/Table/CollapsibleSelectableTable.tsx
@@ -4,23 +4,28 @@ import {
   TableBodyProps as MuiTableBodyProps,
   TableContainer as MuiTableContainer,
   TableHeadProps as MuiTableHeadProps,
+  ToolbarProps as MuiToolbarProps,
 } from "@mui/material";
-import { MouseEvent, useMemo, useState } from "react";
+import { ChangeEvent, MouseEvent, useCallback, useMemo, useState } from "react";
 import TablePagination from "../Pagination/TablePagination";
 import { Order } from "./types";
 import { getComparator } from "./utils";
-
-const defaultRowsPerPageOptions = [5, 10, 15, 20, -1];
 
 type Props<Item> = {
   tableTitle: string;
   initialOrderBy: keyof Item;
   rowsPerPageOptions?: number[];
   data: Item[];
+  TableToolBar?: (
+    props: MuiToolbarProps & { selected: readonly Item[] }
+  ) => JSX.Element;
   TableHead: (
     props: MuiTableHeadProps & {
       order: Order;
       orderBy: keyof Item;
+      numSelected: number;
+      rowCount: number;
+      onSelectAllClick: (event: ChangeEvent<HTMLInputElement>) => void;
       onRequestSort: (event: MouseEvent<unknown>, property: keyof Item) => void;
       isAllRowsOpen: boolean;
       onExpandOrCollapseAllRows: (event: MouseEvent) => void;
@@ -30,23 +35,29 @@ type Props<Item> = {
     props: MuiTableBodyProps & {
       numEmptyRows: number;
       visibleRows: readonly Item[];
+      selected: readonly Item[];
+      updateSelected: (newSelected: readonly Item[]) => void;
       isAllRowsOpen: boolean;
     }
   ) => JSX.Element;
   EmptyTable: () => JSX.Element;
 };
 
-export default function CollapsibleTable<Item>({
+const defaultRowsPerPageOptions = [5, 10, 15, 20, -1];
+
+export default function CollapsibleSelectableTable<Item>({
   tableTitle,
   initialOrderBy,
   rowsPerPageOptions = defaultRowsPerPageOptions,
   data: tableRows,
+  TableToolBar,
   TableHead,
   TableBody,
   EmptyTable,
 }: Props<Item>) {
   const [order, setOrder] = useState<Order>("asc");
   const [orderBy, setOrderBy] = useState<keyof Item>(initialOrderBy);
+  const [selected, setSelected] = useState<readonly Item[]>([]);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(defaultRowsPerPageOptions[0]);
   const [isAllRowsOpen, setIsAllRowsOpen] = useState(false);
@@ -61,15 +72,11 @@ export default function CollapsibleTable<Item>({
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = (newValue: string) => {
-    setRowsPerPage(parseInt(newValue, 10));
-    setPage(0);
+  const updateSelected = (newSelected: readonly Item[]) => {
+    setSelected(newSelected);
   };
 
-  const handleExpandOrCollapseAllRows = () => {
-    setIsAllRowsOpen(!isAllRowsOpen);
-  };
-
+  // Avoid a layout jump when reaching the last page with empty rows.
   const numEmptyRows = Math.max(
     0,
     (1 + page) * Math.min(5, rowsPerPage) - tableRows.length
@@ -89,10 +96,32 @@ export default function CollapsibleTable<Item>({
     [order, orderBy, page, rowsPerPage, tableRows]
   );
 
+  const handleSelectAllClick = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      if (event.target.checked) {
+        setSelected(visibleRows);
+        return;
+      }
+      setSelected([]);
+    },
+    [visibleRows]
+  );
+
+  const handleChangeRowsPerPage = (newValue: string) => {
+    setRowsPerPage(parseInt(newValue, 10));
+    setPage(0);
+  };
+
+  const handleExpandOrCollapseAllRows = () => {
+    setIsAllRowsOpen(!isAllRowsOpen);
+  };
+
   return (
     <MuiBox sx={{ width: "100%" }}>
       {tableRows.length > 0 ? (
         <>
+          {TableToolBar && <TableToolBar selected={selected} />}
+
           <MuiTableContainer>
             <MuiTable
               aria-labelledby={tableTitle}
@@ -101,6 +130,9 @@ export default function CollapsibleTable<Item>({
               <TableHead
                 order={order}
                 orderBy={orderBy}
+                numSelected={selected.length}
+                rowCount={visibleRows.length}
+                onSelectAllClick={handleSelectAllClick}
                 onRequestSort={handleRequestSort}
                 isAllRowsOpen={isAllRowsOpen}
                 onExpandOrCollapseAllRows={handleExpandOrCollapseAllRows}
@@ -108,6 +140,8 @@ export default function CollapsibleTable<Item>({
               <TableBody
                 numEmptyRows={numEmptyRows}
                 visibleRows={visibleRows}
+                selected={selected}
+                updateSelected={updateSelected}
                 isAllRowsOpen={isAllRowsOpen}
               />
             </MuiTable>
