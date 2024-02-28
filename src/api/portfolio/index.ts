4e7a8c0d5b37f345b9d008379f4501eb18cc2727
Jay <chlwodnjs34@gmail.com>
2024-02-28 16:56:24 +0900

    #139 refactor : type 지정 빠진 부분 추가

--- src/api/portfolio/index.ts
+++ src/api/portfolio/index.ts
@@ -5,6 +5,7 @@ import {
   PortfolioPageCharts,
   PortfolioReqBody,
   PortfoliosList,
+  PurchaseHistory,
 } from "./types";
 
 export const getPortfoliosList = async () => {
@@ -69,12 +70,7 @@ export const postPortfolioHolding = async ({
   portfolioId: number;
   body: {
     tickerSymbol: string;
-    purchaseHistory?: {
-      purchaseDate: string;
-      numShares: number;
-      purchasePricePerShare: number;
-      memo: string;
-    };
+    purchaseHistory?: PurchaseHistory;
   };
 }) => {
   const res = await fetcher.post<Response<null>>(
