9214cb47f44aae7334b5087223feb394102d4eeb
Daeram Chung <daeram.chung@gmail.com>
2024-04-09 15:16:18 +0900

    #187 refactor: 개발 모드 시 FCM Service Worker 미작동 문제 해결 - fcmSwEnvPlugin 구현. - 개발 및 빌드 vite config 분리.

--- config/vitePlugins.ts
+++ config/vitePlugins.ts
@@ -1,9 +1,10 @@
+import "dotenv/config";
 import fs from "fs";
 import path from "path";
 
 export function excludeMsw() {
   return {
-    name: "exclude-msw",
+    name: "rollup-plugin-exclude-msw",
     resolveId(source: string) {
       return source === "virtual-module" ? source : null;
     },
@@ -11,6 +12,32 @@ export function excludeMsw() {
       const outDir = outputOptions.dir;
       const msWorker = path.resolve(outDir, "mockServiceWorker.js");
       fs.rm(msWorker, () => {});
+    },
+  };
+}
+
+export function fcmSwEnvPlugin() {
+  return {
+    name: "rollup-plugin-fcm-sw-env",
+    transform(code: string, id: string) {
+      if (id.endsWith("firebase-messaging-sw.js")) {
+        // Replace import.meta.env variables with their actual values
+        const transformedCode = code.replace(
+          new RegExp(`import.meta.env.(\\w+)`, "g"),
+          (_, varName) => `"${process.env[varName]}"`
+        );
+
+        const finalCode =
+          "// IMPORTANT: This file only exists for dev mode purposes. Do not modify this file. Any changes should be made in `src/firebase-messaging-sw.js`.\n\n" +
+          transformedCode;
+
+        const outputPath = path.resolve("public", "./firebase-messaging-sw.js");
+
+        fs.writeFileSync(outputPath, finalCode);
+
+        return null;
+      }
+      return null;
     },
   };
 }
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,21 +1,23 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
-);
+if (typeof importScripts === "function") {
+  importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+  importScripts(
+    "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+  );
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: import.meta.env.VITE_FCM_API_KEY,
-  authDomain: import.meta.env.VITE_FCM_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FCM_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FCM_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FCM_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FCM_APP_ID,
-  measurementId: import.meta.env.VITE_FCM_MEASUREMENT_ID,
-});
+  const firebaseApp = firebase.initializeApp({
+    apiKey: import.meta.env.VITE_FCM_API_KEY,
+    authDomain: import.meta.env.VITE_FCM_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FCM_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FCM_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FCM_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FCM_APP_ID,
+    measurementId: import.meta.env.VITE_FCM_MEASUREMENT_ID,
+  });
 
-firebase.messaging(firebaseApp);
+  firebase.messaging(firebaseApp);
 
-self.addEventListener("install", () => {
-  // eslint-disable-next-line no-console
-  console.log("installed SW!");
-});
+  self.addEventListener("install", () => {
+    // eslint-disable-next-line no-console
+    console.log("installed SW!");
+  });
+}
