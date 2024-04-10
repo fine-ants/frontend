35500b6b6ac83bf914ceecbdeb7ce9cc4d910b8a
Daeram Chung <daeram.chung@gmail.com>
2024-04-10 15:38:06 +0900

    #191 refactor: 예상 월 배당금 차트 호버 시 금액 쉼표 추가

--- src/components/Portfolio/Charts/Dividend/DividendBarTooltip.tsx
+++ src/components/Portfolio/Charts/Dividend/DividendBarTooltip.tsx
@@ -1,20 +1,23 @@
 import designSystem from "@styles/designSystem";
+import { thousandsDelimiter } from "@utils/delimiters";
 import styled from "styled-components";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function DividendBarTooltip({ active, payload, label }: any) {
-  if (active && payload && payload.length) {
+  if (active && payload.length > 0) {
     const currentYear = new Date().getFullYear();
 
     const month = parseInt(label, 10);
-    // 월이 10보다 작으면 앞에 '0'을 붙임
-    const formattedMonth = month.toString().padStart(2, "0");
+    const paddedMonth = month.toString().padStart(2, "0");
+
+    const dividendAmount = payload[0].value;
+
     return (
       <StyledDividendBarTooltip>
         <label>
-          {currentYear}-{formattedMonth}
+          {currentYear}-{paddedMonth}
         </label>
-        <span>{payload[0].value}원</span>
+        <span>₩{thousandsDelimiter(dividendAmount)}</span>
       </StyledDividendBarTooltip>
     );
   }
@@ -23,12 +26,12 @@ export default function DividendBarTooltip({ active, payload, label }: any) {
 }
 
 const StyledDividendBarTooltip = styled.div`
+  padding: 8px;
   display: flex;
   flex-direction: column;
   gap: 4px;
   background-color: ${designSystem.color.neutral.white};
   border-radius: 4px;
-  padding: 8px;
   border: 1px solid ${designSystem.color.neutral.gray100};
   box-shadow: 0px 0px 12px 0px #00000014;
 
