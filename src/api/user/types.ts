745a43f89bdc8bed8e1b3007a5a1cd9c0a102172
Daeram Chung <daeram.chung@gmail.com>
2024-04-17 16:17:48 +0900

    #212 refactor: Only allow account settings tab when local login

--- src/api/user/types.ts
+++ src/api/user/types.ts
@@ -1,8 +1,11 @@
+import { OAuthProvider } from "@api/auth";
+
 export type User = {
   id: number;
   nickname: string;
   email: string;
   profileUrl: string;
+  provider: OAuthProvider | "local";
   notificationPreferences: {
     browserNotify: boolean;
     targetGainNotify: boolean;
