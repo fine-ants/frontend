771b6bd13c9f506f9f7cc9b51a54b381446386f7
Daeram Chung <daeram.chung@gmail.com>
2024-04-19 15:11:19 +0900

    #221 refactor: isFormValid 조건 수정 및 금액 변경 시 퍼센트에 적용되도록 수정

--- src/api/portfolio/queries/usePortfolioEditMutation.ts
+++ src/api/portfolio/queries/usePortfolioEditMutation.ts
@@ -2,7 +2,10 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { putPortfolio } from "..";
 import { portfolioKeys } from "./queryKeys";
 
-export default function usePortfolioEditMutation(portfolioId: number) {
+export default function usePortfolioEditMutation(
+  portfolioId: number,
+  onSuccessCb: () => void
+) {
   const queryClient = useQueryClient();
 
   return useMutation({
@@ -17,9 +20,12 @@ export default function usePortfolioEditMutation(portfolioId: number) {
       queryClient.invalidateQueries({
         queryKey: portfolioKeys.charts(portfolioId).queryKey,
       });
+
+      onSuccessCb();
     },
     meta: {
       toastSuccessMessage: "포트폴리오 수정을 성공했습니다",
+      toastErrorMessage: "포트폴리오 수정을 실패했습니다",
     },
   });
 }
