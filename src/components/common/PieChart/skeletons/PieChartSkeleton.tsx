c9ed5fa6702cb10609b9986ab435f867f431fbb5
Jay <chlwodnjs34@gmail.com>
2024-04-24 15:31:17 +0900

    #232 refactor: 파이차트 스켈레톤 수정 및 레전드 스켈레톤 추가

--- src/components/common/PieChart/skeletons/PieChartSkeleton.tsx
+++ src/components/common/PieChart/skeletons/PieChartSkeleton.tsx
@@ -1,65 +1,35 @@
 import { Skeleton } from "@mui/material";
-import designSystem from "@styles/designSystem";
 import styled from "styled-components";
 
 type Props = {
-  containerWidth: number;
-  containerHeight: number;
-  pieWidth: number;
-  pieHeight: number;
-  innerCircleWidth?: number;
-  innerCircleHeight?: number;
+  size: number;
+  innerSize: number;
 };
 
-export default function PieChartSkeleton({
-  containerWidth,
-  containerHeight,
-  pieWidth,
-  pieHeight,
-  innerCircleWidth,
-  innerCircleHeight,
-}: Props) {
+export function PieChartSkeleton({ size, innerSize }: Props) {
   return (
-    <StyledPieChartSkeleton
-      $containerWidth={containerWidth}
-      $containerHeight={containerHeight}>
-      <Skeleton
-        variant="circular"
-        width={pieWidth}
-        height={pieHeight}
-        style={{ backgroundColor: designSystem.color.primary.blue50 }}
-      />
-      {innerCircleWidth && innerCircleHeight && (
-        <InnerCircle
-          $innerCircleWidth={innerCircleWidth}
-          $innerCircleHeight={innerCircleHeight}
-        />
-      )}
-      {/* TODO: Legend Skeleton */}
+    <StyledPieChartSkeleton>
+      <Skeleton variant="circular" width={size} height={size} />
+      <InnerCircle $width={innerSize} $height={innerSize} />
     </StyledPieChartSkeleton>
   );
 }
 
-const StyledPieChartSkeleton = styled.div<{
-  $containerWidth: number;
-  $containerHeight: number;
-}>`
-  width: ${({ $containerWidth }) => $containerWidth}px;
-  height: ${({ $containerHeight }) => $containerHeight}px;
+const StyledPieChartSkeleton = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   position: relative;
-  border-radius: 8px;
-  background-color: #ffffff;
 `;
 
 const InnerCircle = styled.div<{
-  $innerCircleWidth: number;
-  $innerCircleHeight: number;
+  $width: number;
+  $height: number;
 }>`
-  width: ${({ $innerCircleWidth }) => $innerCircleWidth}px;
-  height: ${({ $innerCircleHeight }) => $innerCircleHeight}px;
+  width: ${({ $width }) => $width}px;
+  height: ${({ $height }) => $height}px;
   position: absolute;
   top: 50%;
   left: 50%;
