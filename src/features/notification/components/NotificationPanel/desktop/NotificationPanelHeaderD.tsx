ebd72a42767b4834e808696c0a38f6708de1377b
Jay <chlwodnjs34@gmail.com>
2024-05-02 00:21:53 +0900

    #248 refactor : 알림 Panel 반응형 적용

---
+++ src/features/notification/components/NotificationPanel/desktop/NotificationPanelHeaderD.tsx
@@ -0,0 +1,88 @@
+import { IconButton } from "@components/Buttons/IconButton";
+import { TextButton } from "@components/Buttons/TextButton";
+import { NotificationSettingsDialog } from "@features/notification/components/NotificaitonSettingsDialog/NotificationSettingsDialog";
+import { User } from "@features/user/api/types";
+import { useBoolean } from "@hooks/useBoolean";
+import designSystem from "@styles/designSystem";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+type Props = {
+  user: User;
+  handleClose: () => void;
+};
+
+export function NotificationPanelHeaderD({ user, handleClose }: Props) {
+  const navigate = useNavigate();
+
+  const {
+    state: isOpenDialog,
+    setTrue: openDialog,
+    setFalse: closeDialog,
+  } = useBoolean();
+
+  const navigateActivateNotify = () => {
+    handleClose();
+    navigate("/notifications/stock");
+  };
+
+  return (
+    <>
+      <PanelHeader>
+        <PanelTitle>알림</PanelTitle>
+        <PanelButtonContainer>
+          <TextButton variant="underline" onClick={navigateActivateNotify}>
+            활성 알림 보기
+          </TextButton>
+          <Divider />
+          <IconButton
+            icon="settings"
+            size="h24"
+            iconColor="custom"
+            customColor={{
+              color: "gray800",
+              hoverColor: "gray50",
+            }}
+            onClick={openDialog}
+          />
+        </PanelButtonContainer>
+      </PanelHeader>
+
+      {isOpenDialog && (
+        <NotificationSettingsDialog
+          user={user}
+          isOpen={isOpenDialog}
+          onClose={closeDialog}
+        />
+      )}
+    </>
+  );
+}
+
+const PanelHeader = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 24px;
+`;
+
+const PanelTitle = styled.div`
+  font: ${designSystem.font.heading3.font};
+  letter-spacing: ${designSystem.font.heading3.letterSpacing};
+  color: ${designSystem.color.neutral.gray800};
+  display: flex;
+  align-items: center;
+`;
+
+const PanelButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+`;
+
+const Divider = styled.div`
+  width: 1px;
+  height: 12px;
+  background-color: ${designSystem.color.neutral.gray200};
+`;
---
+++ src/features/notification/components/NotificationPanel/mobile/NotificationPanelHeaderM.tsx
@@ -0,0 +1,58 @@
+import { IconButton } from "@components/Buttons/IconButton";
+import { MemberNotification } from "@features/notification/api/types";
+import { User } from "@features/user/api/types";
+import designSystem from "@styles/designSystem";
+import styled from "styled-components";
+import NotificationPanelDrawer from "./NotificationPanelDrawer";
+
+type Props = {
+  user: User;
+  notifications: MemberNotification[];
+  hasNotification: boolean;
+  handleClose: () => void;
+};
+
+export function NotificationPanelHeaderM({
+  user,
+  notifications,
+  hasNotification,
+  handleClose,
+}: Props) {
+  const notificationIds = notifications.map((data) => data.notificationId);
+
+  return (
+    <>
+      <PanelHeader>
+        <IconButton
+          icon="arrow-left"
+          size="h40"
+          iconColor="gray"
+          onClick={handleClose}
+        />
+        <PanelTitle>알림</PanelTitle>
+        <NotificationPanelDrawer
+          user={user}
+          notificationIds={notificationIds}
+          hasNotification={hasNotification}
+        />
+      </PanelHeader>
+    </>
+  );
+}
+
+const PanelHeader = styled.header`
+  width: 100%;
+  height: 56px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 8px;
+`;
+
+const PanelTitle = styled.div`
+  font: ${designSystem.font.title3.font};
+  letter-spacing: ${designSystem.font.title3.letterSpacing};
+  color: ${designSystem.color.neutral.gray800};
+  display: flex;
+  align-items: center;
+`;
