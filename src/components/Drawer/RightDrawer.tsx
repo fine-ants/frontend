1614301174b9397b597a6b8fd918be7be1dfa88e
Daeram Chung <daeram.chung@gmail.com>
2024-05-17 14:59:24 +0900

    #268 feat: Create and apply RightDrawer component

---
+++ src/components/Drawer/RightDrawer.tsx
@@ -0,0 +1,42 @@
+import { SwipeableDrawer, ThemeProvider, createTheme } from "@mui/material";
+import { ReactNode } from "react";
+
+type Props = {
+  isDrawerOpen: boolean;
+  onOpenDrawer: () => void;
+  onCloseDrawer: () => void;
+  children: ReactNode;
+};
+
+export default function RightDrawer({
+  isDrawerOpen,
+  onOpenDrawer,
+  onCloseDrawer,
+  children,
+}: Props) {
+  return (
+    <ThemeProvider theme={theme}>
+      <SwipeableDrawer
+        anchor="right"
+        open={isDrawerOpen}
+        onOpen={onOpenDrawer}
+        onClose={onCloseDrawer}>
+        {children}
+      </SwipeableDrawer>
+    </ThemeProvider>
+  );
+}
+
+const theme = createTheme({
+  components: {
+    MuiDrawer: {
+      styleOverrides: {
+        root: {
+          ".MuiPaper-root": {
+            width: "100%",
+          },
+        },
+      },
+    },
+  },
+});
