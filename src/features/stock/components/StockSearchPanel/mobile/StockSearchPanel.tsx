8fff4d7da6acf3e62760aaed41d2cb27e269037d
Daeram Chung <daeram.chung@gmail.com>
2024-05-17 21:49:06 +0900

    #270 refactor: Unseparate StockSearchPanel header and content

--- src/features/stock/components/StockSearchPanel/mobile/StockSearchPanel.tsx
+++ src/features/stock/components/StockSearchPanel/mobile/StockSearchPanel.tsx
@@ -1,7 +1,8 @@
+import { IconButton } from "@components/Buttons/IconButton";
 import RightDrawer from "@components/Drawer/RightDrawer";
+import SearchBarM from "@components/SearchBar/mobile/SearchBarM";
+import designSystem from "@styles/designSystem";
 import styled from "styled-components";
-import StockSearchPanelContext from "./StockSearchPanelContent";
-import StockSearchPanelHeader from "./StockSearchPanelHeader";
 
 type Props = {
   isPanelOpen: boolean;
@@ -20,8 +21,20 @@ export default function StockSearchPanel({
       onOpenDrawer={onOpenPanel}
       onCloseDrawer={onClosePanel}>
       <StyledStockSearchPanel>
-        <StockSearchPanelHeader handleClose={onClosePanel} />
-        <StockSearchPanelContext />
+        <PanelHeader>
+          <IconButton
+            icon="arrow-left"
+            size="h40"
+            iconColor="custom"
+            customColor={{ color: "gray800", hoverColor: "gray50" }}
+            onClick={onClosePanel}
+          />
+          <PanelTitle>검색</PanelTitle>
+        </PanelHeader>
+
+        <PanelContent>
+          <SearchBarM />
+        </PanelContent>
       </StyledStockSearchPanel>
     </RightDrawer>
   );
@@ -32,4 +45,27 @@ const StyledStockSearchPanel = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
+`;
+
+const PanelHeader = styled.header`
+  width: 100%;
+  padding: 16px;
+  display: flex;
+  position: relative;
+`;
+
+const PanelTitle = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font: ${designSystem.font.title3.font};
+  letter-spacing: ${designSystem.font.title3.letterSpacing};
+  color: ${designSystem.color.neutral.gray900};
+`;
+
+const PanelContent = styled.div`
+  width: 100%;
 `;
