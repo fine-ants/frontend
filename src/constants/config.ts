6372973794d37ed7d88d5959964cde7b15ad0357
Daeram Chung <daeram.chung@gmail.com>
2024-05-28 15:10:14 +0900

    #291 refactor: Remove unnecessary constant and add missing toast error message

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -7,8 +7,3 @@ export const BASE_API_URL =
   process.env.NODE_ENV === "production"
     ? import.meta.env.VITE_API_URL_PROD
     : import.meta.env.VITE_API_URL_DEV;
-
-export const BASE_API_URL_WS =
-  process.env.NODE_ENV === "production"
-    ? import.meta.env.VITE_API_URL_PROD_WS
-    : import.meta.env.VITE_API_URL_DEV_WS;
