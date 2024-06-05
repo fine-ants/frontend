7dc2047842f09dd386f5c72e0deef387a9df1090
Jay <chlwodnjs34@gmail.com>
2024-06-05 14:58:20 +0900

    #308 refactor : 불필요한 fragment 삭제

--- src/components/CardTable/SelectableCardTable/SelectableCardTableToolbar.tsx
+++ src/components/CardTable/SelectableCardTable/SelectableCardTableToolbar.tsx
@@ -23,41 +23,39 @@ export function SelectableCardTableToolbar({
   openDrawer,
 }: Props) {
   return (
-    <>
-      <Toolbar>
-        <Controller>
-          <CheckBox
-            size="h20"
-            onChange={onSelectAllClick}
-            checked={isAllRowsSelectedInCurrentPage}
-          />
+    <Toolbar>
+      <Controller>
+        <CheckBox
+          size="h20"
+          onChange={onSelectAllClick}
+          checked={isAllRowsSelectedInCurrentPage}
+        />
 
-          {numSelected > 0 && (
-            <>
-              <Typography
-                sx={{ font: designSystem.font.body3.font }}
-                color="inherit"
-                variant="subtitle1"
-                component="span">
-                <span>{numSelected}</span>
-                <span style={{ color: designSystem.color.neutral.gray600 }}>
-                  개 선택됨
-                </span>
-              </Typography>
-              <Icon icon="divider" size={12} color="gray100" />
-              <Button variant="tertiary" size="h32" onClick={openDeleteConfirm}>
-                <Icon icon="trash" size={16} color="gray600" />
-                <span>삭제</span>
-              </Button>
-            </>
-          )}
-        </Controller>
-        <TextButton size="h24" color="primary" onClick={openDrawer}>
-          <Icon icon="ascending" size={16} color="blue500" />
-          정렬
-        </TextButton>
-      </Toolbar>
-    </>
+        {numSelected > 0 && (
+          <>
+            <Typography
+              sx={{ font: designSystem.font.body3.font }}
+              color="inherit"
+              variant="subtitle1"
+              component="span">
+              <span>{numSelected}</span>
+              <span style={{ color: designSystem.color.neutral.gray600 }}>
+                개 선택됨
+              </span>
+            </Typography>
+            <Icon icon="divider" size={12} color="gray100" />
+            <Button variant="tertiary" size="h32" onClick={openDeleteConfirm}>
+              <Icon icon="trash" size={16} color="gray600" />
+              <span>삭제</span>
+            </Button>
+          </>
+        )}
+      </Controller>
+      <TextButton size="h24" color="primary" onClick={openDrawer}>
+        <Icon icon="ascending" size={16} color="blue500" />
+        정렬
+      </TextButton>
+    </Toolbar>
   );
 }
 
