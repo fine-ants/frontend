d9db1794d2723ade63ad5a78099cdfd971b1aca9
Jay <chlwodnjs34@gmail.com>
2024-06-26 18:01:18 +0900

    #324 refactor : 불필요한 줄바꿈 제거

--- src/features/portfolio/components/PortfolioHolding/mobile/PortfolioHoldingCardTable.tsx
+++ src/features/portfolio/components/PortfolioHolding/mobile/PortfolioHoldingCardTable.tsx
@@ -11,28 +11,13 @@ type Props = {
 export default function PortfolioHoldingCardTable({ data }: Props) {
   const orderByList: { title: string; orderBy: keyof PortfolioHolding }[] = [
     { title: "종목명", orderBy: "companyName" },
-    {
-      title: "평가 금액",
-      orderBy: "currentValuation",
-    },
+    { title: "평가 금액", orderBy: "currentValuation" },
     { title: "현재가", orderBy: "currentPrice" },
     { title: "평균 매입가", orderBy: "averageCostPerShare" },
-    {
-      title: "개수",
-      orderBy: "numShares",
-    },
-    {
-      title: "변동률",
-      orderBy: "dailyChangeRate",
-    },
-    {
-      title: "총 손익",
-      orderBy: "totalGain",
-    },
-    {
-      title: "연 배당금",
-      orderBy: "annualDividend",
-    },
+    { title: "개수", orderBy: "numShares" },
+    { title: "변동률", orderBy: "dailyChangeRate" },
+    { title: "총 손익", orderBy: "totalGain" },
+    { title: "연 배당금", orderBy: "annualDividend" },
   ];
 
   return (
