2c034d5cf3a9011d1e65c3a11859a8c2940fa551
Jay <chlwodnjs34@gmail.com>
2024-07-02 17:17:37 +0900

    #328 refactor : Toast error message를 onError로 이동

--- src/features/watchlist/api/queries/useWatchlistItemAddMutation.ts
+++ src/features/watchlist/api/queries/useWatchlistItemAddMutation.ts
@@ -1,4 +1,7 @@
+import { Response } from "@api/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { toast } from "src/main";
 import { postWatchlistStocks } from "..";
 import { watchlistKeys } from "./queryKeys";
 
@@ -21,9 +24,13 @@ export default function useWatchlistItemAddMutation(
         queryKey: watchlistKeys.hasStock.queryKey,
       });
     },
+    onError: (error) => {
+      const message = (error as AxiosError<Response<null>>).response?.data
+        ?.message as string;
+      toast.error(message);
+    },
     meta: {
       toastSuccessMessage: "관심 종목을 추가했습니다",
-      toastErrorMessage: "관심 종목을 추가하는 중 오류가 발생했습니다",
     },
   });
 }
