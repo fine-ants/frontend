30d3c5ac8a565333272118b2351241d9d715ef44
jay <chlwodnjs34@gmail.com>
2024-07-05 16:39:05 +0900

    #332 refactor : 포트폴리오 매입이력 반응형 작업 추가

--- src/features/portfolio/components/PortfolioHolding/mobile/PortfolioHoldingLots/PortfolioHoldingLotsTableM.tsx
+++ src/features/portfolio/components/PortfolioHolding/mobile/PortfolioHoldingLots/PortfolioHoldingLotsTableM.tsx
@@ -1,5 +1,11 @@
+import { TextButton } from "@components/Buttons/TextButton";
+import { Icon } from "@components/Icon";
 import { PurchaseHistory } from "@features/portfolio/api/types";
+import { useBoolean } from "@fineants/demolition";
+import { Divider } from "@mui/material";
+import designSystem from "@styles/designSystem";
 import styled from "styled-components";
+import PortfolioHoldingAddOrEditLots from "./PortfolioHoldingAddOrEditLots";
 import PortfolioHoldingLotsCard from "./PortfolioHoldingLotsCard";
 
 type Props = {
@@ -11,6 +17,12 @@ export default function PortfolioHoldingLotsTableM({
   portfolioHoldingId,
   purchaseHistory,
 }: Props) {
+  const {
+    state: isOpenAddLots,
+    setTrue: onAddLotsOpen,
+    setFalse: onAddLotsClose,
+  } = useBoolean();
+
   return (
     <StyledPortfolioHoldingLotsTable>
       {purchaseHistory.map((lot) => (
@@ -20,6 +32,27 @@ export default function PortfolioHoldingLotsTableM({
           lot={lot}
         />
       ))}
+
+      {isOpenAddLots && (
+        <>
+          <StyledPortfolioHoldingLotsCard>
+            <PortfolioHoldingAddOrEditLots
+              portfolioHoldingId={portfolioHoldingId}
+              onDeleteConfirmAlertOpen={() => {}}
+              onClose={onAddLotsClose}
+            />
+          </StyledPortfolioHoldingLotsCard>
+
+          <StyledDivider />
+        </>
+      )}
+
+      <ButtonWrapper>
+        <TextButton size="h24" color="primary" onClick={onAddLotsOpen}>
+          <Icon icon="add" size={16} color="blue500" />
+          항목 추가
+        </TextButton>
+      </ButtonWrapper>
     </StyledPortfolioHoldingLotsTable>
   );
 }
@@ -29,4 +62,22 @@ const StyledPortfolioHoldingLotsTable = styled.div`
   flex-direction: column;
   gap: 16px;
   margin-top: 8px;
+`;
+
+const ButtonWrapper = styled.div`
+  margin-top: 8px;
+  margin-left: auto;
+`;
+
+const StyledPortfolioHoldingLotsCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding-left: 16px;
+  border-left: 1px solid ${designSystem.color.neutral.gray100};
+`;
+
+const StyledDivider = styled(Divider)`
+  margin-top: 16px;
+  border-color: ${designSystem.color.neutral.gray200};
 `;
