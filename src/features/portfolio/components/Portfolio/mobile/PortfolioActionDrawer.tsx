55204fc9be47aa57a5895ac31fe3a25ccf62677c
Daeram Chung <daeram.chung@gmail.com>
2024-07-12 17:16:47 +0900

    #340 refactor: Create and apply DrawerItem for BottomDrawer

--- src/features/portfolio/components/Portfolio/mobile/PortfolioActionDrawer.tsx
+++ src/features/portfolio/components/Portfolio/mobile/PortfolioActionDrawer.tsx
@@ -1,5 +1,6 @@
 import ConfirmAlert from "@components/ConfirmAlert";
 import BottomDrawer from "@components/Drawer/BottomDrawer";
+import DrawerItem from "@components/Drawer/DrawerItem";
 import { Icon } from "@components/Icon";
 import { useBoolean } from "@fineants/demolition";
 import Routes from "@router/Routes";
@@ -52,30 +53,22 @@ export default function PortfolioActionDrawer({
         onOpenDrawer={onDrawerOpen}
         onCloseDrawer={onDrawerClose}>
         <ul>
-          <ContentItem>
-            <ContentItemButton
-              onClick={() => {
-                onDrawerClose();
-                onDialogOpen();
-              }}>
-              <ContentWrapper>
-                <Icon icon="edit" size={16} color="gray400" />
-                <ItemTitle>편집</ItemTitle>
-              </ContentWrapper>
-            </ContentItemButton>
-          </ContentItem>
-          <ContentItem>
-            <ContentItemButton
-              onClick={() => {
-                onDrawerClose();
-                onConfirmAlertOpen();
-              }}>
-              <ContentWrapper>
-                <Icon icon="trash" size={16} color="gray400" />
-                <ItemTitle>삭제</ItemTitle>
-              </ContentWrapper>
-            </ContentItemButton>
-          </ContentItem>
+          <DrawerItem
+            onClick={() => {
+              onDrawerClose();
+              onDialogOpen();
+            }}>
+            <Icon icon="edit" size={16} color="gray400" />
+            <ItemTitle>편집</ItemTitle>
+          </DrawerItem>
+          <DrawerItem
+            onClick={() => {
+              onDrawerClose();
+              onConfirmAlertOpen();
+            }}>
+            <Icon icon="trash" size={16} color="gray400" />
+            <ItemTitle>삭제</ItemTitle>
+          </DrawerItem>
         </ul>
       </BottomDrawer>
 
@@ -99,34 +92,6 @@ export default function PortfolioActionDrawer({
   );
 }
 
-const ContentItem = styled.li`
-  width: 100%;
-  height: 56px;
-  display: flex;
-  align-items: center;
-  padding: 0 16px;
-
-  &:active {
-    background-color: ${designSystem.color.neutral.gray50};
-  }
-`;
-
-const ContentItemButton = styled.button`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font: ${designSystem.font.title4.font};
-  letter-spacing: ${designSystem.font.title4.letterSpacing};
-`;
-
 const ItemTitle = styled.span`
   color: ${designSystem.color.neutral.gray800};
-`;
-
-const ContentWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
 `;
--- src/features/watchlist/components/WatchlistTable/mobile/WatchlistActionDrawer.tsx
+++ src/features/watchlist/components/WatchlistTable/mobile/WatchlistActionDrawer.tsx
@@ -1,4 +1,5 @@
 import BottomDrawer from "@components/Drawer/BottomDrawer";
+import DrawerItem from "@components/Drawer/DrawerItem";
 import { Icon } from "@components/Icon";
 import designSystem from "@styles/designSystem";
 import styled from "styled-components";
@@ -24,63 +25,27 @@ export default function WatchlistActionDrawer({
       onOpenDrawer={onDrawerOpen}
       onCloseDrawer={onDrawerClose}>
       <ul>
-        <ContentItem>
-          <ContentItemButton
-            onClick={() => {
-              onDrawerClose();
-              onNameEditDialogOpen();
-            }}>
-            <ContentWrapper>
-              <Icon icon="edit" size={16} color="gray400" />
-              <ItemTitle>편집</ItemTitle>
-            </ContentWrapper>
-          </ContentItemButton>
-        </ContentItem>
-        <ContentItem>
-          <ContentItemButton
-            onClick={() => {
-              onDrawerClose();
-              onDeleteWatchlistAlertOpen();
-            }}>
-            <ContentWrapper>
-              <Icon icon="trash" size={16} color="gray400" />
-              <ItemTitle>삭제</ItemTitle>
-            </ContentWrapper>
-          </ContentItemButton>
-        </ContentItem>
+        <DrawerItem
+          onClick={() => {
+            onDrawerClose();
+            onNameEditDialogOpen();
+          }}>
+          <Icon icon="edit" size={16} color="gray400" />
+          <ItemTitle>편집</ItemTitle>
+        </DrawerItem>
+        <DrawerItem
+          onClick={() => {
+            onDrawerClose();
+            onDeleteWatchlistAlertOpen();
+          }}>
+          <Icon icon="trash" size={16} color="gray400" />
+          <ItemTitle>삭제</ItemTitle>
+        </DrawerItem>
       </ul>
     </BottomDrawer>
   );
 }
 
-const ContentItem = styled.li`
-  width: 100%;
-  height: 56px;
-  display: flex;
-  align-items: center;
-  padding: 0 16px;
-
-  &:active {
-    background-color: ${designSystem.color.neutral.gray50};
-  }
-`;
-
-const ContentItemButton = styled.button`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font: ${designSystem.font.title4.font};
-  letter-spacing: ${designSystem.font.title4.letterSpacing};
-`;
-
 const ItemTitle = styled.span`
   color: ${designSystem.color.neutral.gray800};
-`;
-
-const ContentWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
 `;
